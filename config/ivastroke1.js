c3e45d0fbbf83fdd5810b7f29797005a7a2947c5
BahmanMirheidari <bmirheidari@yahoo.com>
2020-06-17 11:46:01 +0100

    iva3 testing question details

--- config/ivastroke1.js
+++ config/ivastroke1.js
@@ -1,115 +1,157 @@
-// config/iva3.js
+// config/ivastroke1.js
 module.exports = { 
         'consent': {},
         'questions':[
             {
+                'q_no':0,
+                'text':'Hello, I am a computerised doctor and I’ll be asking you questions today. I will ask you the sorts of questions doctors ask in a memory clinic. Thank you for talking to me. I will start to ask questions shortly.',
+                'length':15000,
+                'video_url':'',
+                'imge_url':'',
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false 
+            },
+            {
                 'q_no':1,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Please read the following paragraph',
+                'length':4000,
                 'video_url':'',
                 'imge_url':'',
                 'show_text':true,
+                'show_image':false,
                 'play_buzzer':false 
             },
             {
                 'q_no':2,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Tell me what problems you have noticed with your memory recently.',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
             },
             {
                 'q_no':3,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Who is most worried about your memory you or somebody else?',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
             },
             {
                 'q_no':4,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
+                'text':'What did you do over last weekend, giving as much detail as you can?',
                 'length':0,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
             },
             {
                 'q_no':5,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'What has been in the news recently?',
+                'length':4000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
             },
             {
                 'q_no':6,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Tell me what you did when you left school. What jobs did you do?',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
+                'show_text':false,
+                'show_image':false,
                 'play_buzzer':false 
             },
             {
                 'q_no':7,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Tell me about your last job or your current job.',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
+                'show_text':false,
+                'show_image':false,
                 'play_buzzer':false 
             },
             {
                 'q_no':8,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Who manages your finances? You or somebody else? ',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
+                'show_text':false,
+                'show_image':false,
                 'play_buzzer':false 
             },
             {
                 'q_no':9,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Please name as many animals as you can. You can name any type of animal you will have one minute. Please start after you hear the buzzer.',
+                'length':12000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':true 
             },
             {
                 'q_no':10,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Please name as many words as you can that begin with the letter p. It can be any word beginning with p except for names of people such as Peter or names of countries such as Portugal. Please start answering after you hear the buzzer.',
+                'length':14000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':true
             },
             {
                 'q_no':11,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Tell me everything you see going on in this picture. Please describe it in as much detail as you can. When you have finished press forward.',
+                'length':10000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
+                'show_text':false,
+                'show_image':true,
                 'play_buzzer':false 
-            },
+            }, 
             {
                 'q_no':12,
-                'text':'WHERE HAVE YOU COME IN FROM TODAY AND WHAT ARE YOU HOPING TO FIND OUT',
-                'length':0,
+                'text':'Please say the months of the year in reverse order.',
+                'length':5000,
                 'video_url':'',
                 'imge_url':'',
-                'show_text':true,
-                'play_buzzer':false 
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
             },
+            {
+                'q_no':13,
+                'text':'Could you tell how to make a cup of tea? Please describe it step by step.',
+                'length':6000,
+                'video_url':'',
+                'imge_url':'',
+                'show_text':false,
+                'show_image':false,
+                'play_buzzer':false
+            }, 
+            {
+                'q_no':14,
+                'text':'THANK YOU FOR TAKING PART THE TRIAL IS NOW OVER',
+                'length':4000,
+                'video_url':'',
+                'imge_url':'',
+                'show_text':false,
+                'show_image':true,
+                'play_buzzer':false 
+            }
 
         ], 
         'surveys': []  
