c29f72ca492a103c1525c324adce016f3f75304e
BahmanMirheidari <bmirheidari@yahoo.com>
2020-06-20 18:47:45 +0100

    talk2iva

--- app_8_6_20.js
+++ app_8_6_20.js
@@ -305,7 +305,9 @@ var httpsServer = ( config.ssl ) ? https.createServer({key: fs.readFileSync(conf
 httpsServer.listen(config.port); 
 
 ( config.ssl ) ? logger.info('https server (' + ip_address + ') is running on port: ' + config.port): logger.info('http server (' + ip_address + ') is running on port: ' + config.port);
-  
+
+logger.info(config.iva_configs[config.iva_default].consent.title)
+
 var wss = new WebSocketServer({
 server: httpsServer
 });
