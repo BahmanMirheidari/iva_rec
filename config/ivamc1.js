8b711d5e92d2600637f7e090b8a9acc506a296cc
BahmanMirheidari <bmirheidari@yahoo.com>
2020-08-03 09:41:40 +0100

    missed comma

--- config/ivamc1.js
+++ config/ivamc1.js
@@ -3,7 +3,7 @@ shared_conf    = require( './shared_conf.js' );
 
 module.exports = { 
     'consent': {},
-    'pre_surveys': [shared_conf.survey_anosognosia_pat, shared_conf.survey_anosognosia_carer]
+    'pre_surveys': [shared_conf.survey_anosognosia_pat, shared_conf.survey_anosognosia_carer],
     'questions': shared_conf.questions_iva3, 
     'surveys': [shared_conf.survey_phq8, shared_conf.survey_gad7]
 };
