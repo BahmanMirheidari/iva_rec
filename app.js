4b3c0f05db818d51a7b3c92ec343868552d80f4e
BahmanMirheidari <bmirheidari@yahoo.com>
2020-08-20 14:28:53 +0100

    href

--- app.js
+++ app.js
@@ -232,7 +232,6 @@ app.get('/editparticipant/:id', ensureIsClinician, editparticipantPage);
 app.get('/editparticipantpass/:id', ensureIsClinician, editparticipantpassPage);
 
 app.get('/deleteparticipant/:id', ensureIsClinician, deleteparticipant);
-app.post('/participant', ensureIsClinician, getparticipantHomePage);
 app.post('/addparticipant', ensureIsClinician, addparticipant);
 app.post('/editparticipant/:id', ensureIsClinician, editparticipant); 
 app.post('/editparticipantpass/:id', ensureIsClinician, editparticipantpass);   
