c758677d1262ee939bc2aef833b3197705aba596
BahmanMirheidari <bmirheidari@yahoo.com>
2020-11-10 22:21:24 +0000

    guid

--- controllers/participant.js
+++ controllers/participant.js
@@ -3,20 +3,12 @@
 shared = require( './shared' );
 var pageSize = 10;
 
-function generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
-        }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+function S4() {
+      return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+   }  
+
+function guid(){
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
 }
 
 
@@ -133,11 +125,11 @@ module.exports = {
                 let uploadedFile = req.files.filename;
 
                 console.log(uploadedFile.name);
-                let guid = generateUUID();
-                
-                console.log(guid);
+                let g = guid();
 
-                let newname = 'tmp/' + guid + '-' + uploadedFile.name;
+                console.log(g);
+
+                let newname = 'tmp/' + g + '-' + uploadedFile.name;
                 uploadedFile.mv(newname);
 
                 // read contents of the file
