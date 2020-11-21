e750622590a7f4ce22130549d0825e9e3e4fe537
BahmanMirheidari <bmirheidari@yahoo.com>
2020-11-21 00:12:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -479,7 +479,7 @@ wss.on('connection', function connection(ws) {
                 else if (msg == 'survey')
                     common.process_survey(data, __dirname, config.mount_dir);
                 else if (msg == 'segment')
-                    handelSegment(data);
+                    handelSegment(message);
                 else if (msg == 'video')
                     handleWebmVideoAudio(data,'video',500000000); 
                 else if (msg == 'audio')
