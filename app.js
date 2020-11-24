fc5a710da5b02b3ef95456ca8820ecee52bf7845
BahmanMirheidari <bmirheidari@yahoo.com>
2020-11-24 22:03:25 +0000

    Update app.js

--- app.js
+++ app.js
@@ -460,13 +460,13 @@ var wss = new WebSocketServer({
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        message = JSON.parse(message);
-        var received_ip = ws._socket.remoteAddress;  //ws._socket.remoteAddress;
-        var msg = message.msg;
-        var data = message.data; 
-        var osBrStr = 'ip:'+received_ip+', os:'+message.os+', browser:'+message.browser+', date:'+message.date.toString();
-
         try {
+            message = JSON.parse(message);
+            var received_ip = ws._socket.remoteAddress;  //ws._socket.remoteAddress;
+            var msg = message.msg;
+            var data = message.data; 
+            var osBrStr = 'ip:'+received_ip+', os:'+message.os+', browser:'+message.browser+', date:'+message.date.toString();
+    
             if (msg != null) {
 
                 logger.info('received ip: ' + received_ip + ' - msg: ' + msg);
