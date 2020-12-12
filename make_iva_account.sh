5622465123409e515cede1dfafd63906b4f1e6c4
BahmanMirheidari <bmirheidari@yahoo.com>
2020-12-12 10:23:28 +0000

    Update make_iva_account.sh

--- make_iva_account.sh
+++ make_iva_account.sh
@@ -35,5 +35,5 @@ if [[ "$#" -ge 7 ]];then
 else
 	echo "builds a list of new accounts for the IVA website ..."
 	echo "usage $0 <n:50> <p_start:260> <u_start:283> <r_start:20> <r_prefix:S> <repeat:ivastroke1|.R1:ivastroke2|.R2:ivastroke1|.R3:ivastroke2> <r_len:4> <len:12>" 
-	echo "e.g. $0 50 260 283 20 S \":ivastroke1|.R1:ivastroke2|.R2:ivastroke1|.R3:ivastroke2"" 4 12" 
+	echo "e.g. $0 50 260 283 20 S \":ivastroke1|.R1:ivastroke2|.R2:ivastroke1|.R3:ivastroke2\" 4 12" 
 fi
